60e1adf38697bf78f653a43b0ccc386b337bcb21
Karan Patil <karanp0202@gmail.com>
2021-07-10 18:27:01 +0530

    script management

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-import("./Web/js/snake.js");
-
 class Vector3 {
-    angleX = 0;
-    angleY = 0;
-    angleZ = 0;
+    constructor(X, Y, Z) {
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
 }
 
-let angle = new Vector3;
+class Snake {
+    constructor() {
+        this.pos = new Vector3(0,0,-21);
+    }
+    renderFace = () => {
+        translate(0, -20, 0);
+        translate(-this.pos.X, -this.pos.Y, -this.pos.Z);
+        box(2);
+        translate(this.pos.X, this.pos.Y,this.pos.Z);
+    }
+}
+
+class Cube {
+    constructor(X,Y,Z) {
+        this.sizeX = X;
+        this.sizeY = Y;
+        this.sizeZ = Z;
+        this.angle = new Vector3(0, 0, 0);
+    }
+    renderCube() {
+        noStroke();
+        
+        translate(0, 0, this.sizeZ / 2);
+        fill(252, 142, 134); box(this.sizeX, this.sizeY, 0);
+        translate(0, 0, -this.sizeZ);
+        fill(252, 240, 134); box(this.sizeX, this.sizeY, 0);
+        translate(this.sizeX / 2, 0, this.sizeZ / 2);
+        fill(134, 252, 167); box(0, this.sizeY, this.sizeZ);
+        translate(-this.sizeX, 0, 0);
+        fill(154, 183, 255); box(0, this.sizeY, this.sizeZ);
+        translate(this.sizeX / 2, -this.sizeY / 2, 0);
+        fill(219, 134, 252); box(this.sizeX, 0, this.sizeZ);
+        translate(0, this.sizeY, 0);
+        fill(252, 134, 197); box(this.sizeX, 0, this.sizeZ);
+    }
+    rotateCube() {
+        this.angle.X += 0.01;
+        this.angle.Y += 0.01;
+        this.angle.Z += 0.01;
+    }
+}
+
+this.snake = new Snake;
+this.cube = new Cube(40, 40, 40);
 
 function setup() {
-    createCanvas(800, 500, WEBGL);
-    perspective(PI / 5.0, width / height, 0.1, 5000);
-}
-
-function mousePressed() {
-
+    createCanvas(600, 600, WEBGL);
+    const canvas = document.getElementById("defaultCanvas0");
+    perspective(PI / 5.0, width / height, 0.1, 500);
 }
 
 function draw() {
     clear();
-    translate(0,0, -200);
-    rotateX(angle.angleX);
-    rotateY(angle.angleY);
-    rotateZ(angle.angleZ);
-    noStroke();
-    baseCube();
-    rotateGL();
-}
+    translate(0,0, 380);
+    rotateX(cube.angle.X);
+    rotateY(cube.angle.Y);
+    rotateZ(cube.angle.Z);
 
-function baseCube() {
-    translate(0,0, 100);
-    fill(252, 142, 134);
-    box(200, 200, 0);
-
-    translate(0,0, -200);
-    fill(252, 240, 134);
-    box(200, 200, 0);
-
-    translate(100,0, 100);
-    fill(134, 252, 167);
-    box(0, 200, 200);
+    cube.renderCube();
     
-    translate(-200,0, 0);
-    fill(154, 183, 255);
-    box(0, 200, 200);
+    fill(0, 0, 0);
+    snake.renderFace();
 
-    translate(100, -100, 0);
-    fill(219, 134, 252);
-    box(200, 0, 200);
-
-    translate(0, 200, 0);
-    fill(252, 134, 197);
-    box(200, 0, 200);
-}
-
-function rotateGL() {
-    angle.angleX += 0.01;
-    angle.angleY += 0.01;
-    angle.angleZ += 0.01;
+    cube.rotateCube();
 }
